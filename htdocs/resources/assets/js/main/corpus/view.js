abbbac67fe9c1ebf16edbf72c5d26e70ab44a498
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-07-18 09:29:38 +0900

    [add] コーパス管理画面用のjs追加

---
+++ htdocs/resources/assets/js/main/corpus/view.js
@@ -0,0 +1,28 @@
+// デバッグ用
+const debug = true;
+
+function logInfo(msg) {
+  if(debug) console.log(msg);
+}
+
+
+/**
+ * コーパス作成モーダル制御
+ */
+function setCreateCorpusModal() {
+
+
+}
+
+
+function initialize() {
+  // setCreateCorpusModal();
+}
+
+// 
+(function() {
+  //
+  logInfo('[Start] view.js');
+  initialize();
+
+})();
