e50e5085239c6d712e3ad34708f9e75fa7e7df1f
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-07-18 09:27:13 +0900

    [update] コーパスのモデルに、コーパス新規作成時のバリデーション追加

--- htdocs/app/Models/Corpus.php
+++ htdocs/app/Models/Corpus.php
@@ -6,6 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Corpus extends Model
 {
+
+    /**
+     * 新規作成バリデーション
+     */
+    public static $create_rule = array(
+        'name' => 'required|between:0,191',
+        'description' => 'required|between:0,191',
+        'language' => 'required'
+    );
+
+    public static $create_error_messages = array(
+        'name.required' => 'コーパス名が入力されていません',
+        'name.between' => 'コーパス名は191文字以内で入力してください',
+        'description.required' => '説明文が入力されていません',
+        'description.between' => '説明文は191文字以内で入力してください',
+        'language.required' => '言語を指定してください'
+    );
+
+
+
     /**
      * このコーパスを所有するAPIを取得
      */
