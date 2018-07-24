59807b443acfa3cd0c9a74915f89f0deaeadf264
TakuKaneko <takuya.kaneko.4192@gmail.com>
2018-07-24 13:53:54 +0900

    [update]@kaneko ログイン画面のデザイン修正

--- htdocs/resources/views/auth/login.blade.php
+++ htdocs/resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="csrf-token" content="{{ csrf_token() }}"> 
   <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <link rel="stylesheet" type="text/css" href="{{ mix('/css/main/dashboard/login.css') }}" />
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
