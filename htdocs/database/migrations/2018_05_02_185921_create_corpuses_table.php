dd4d26595576d073739f0fb7b5a76e078a1ea603
Takahiro Fukui <takfukui@pasonatech.co.jp>
2018-07-27 09:25:56 +0900

    [update] コーパスの説明文のカラムの文字数を255に更新

--- htdocs/database/migrations/2018_05_02_185921_create_corpuses_table.php
+++ htdocs/database/migrations/2018_05_02_185921_create_corpuses_table.php
@@ -16,7 +16,7 @@ class CreateCorpusesTable extends Migration
         Schema::create('corpuses', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable(false);
-            $table->string('description');
+            $table->string('description', 255);
             $table->string('status');
             $table->integer('type');
             $table->string('is_production');
